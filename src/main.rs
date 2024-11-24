06a57544f62e2ee6d13eeba54b4f2f37e2bc8669
droidroot <goryakin@phystech.edu>
2024-11-24 17:58:25 +0300

    initial text input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use iced::{widget::button, widget::Column, widget::Text};
+use iced::{widget::{button, text_input, Column, Row, Text}, Alignment::Center, Element};
 
 fn main() -> iced::Result {
     iced::run("Number visualizer", Visualizer::update, Visualizer::view)
@@ -6,12 +6,14 @@ fn main() -> iced::Result {
 
 #[derive(Default)]
 struct Visualizer {
+    number: String,
     count: u32,
 }
 
 #[derive(Debug, Clone)]
 enum Message {
     ButtonPressed,
+    ContentChanged(String),
 }
 
 impl Visualizer {
@@ -19,18 +21,33 @@ impl Visualizer {
         match message {
             Message::ButtonPressed => {
                 self.count += 1;
-            }
+            },
+            Message::ContentChanged(content) => {
+                self.number = content;
+            },
         }
     }
 
-    fn view(&self) -> Column<Message> {
-        let button = button("Press me!").on_press(Message::ButtonPressed);
+    fn view(&self) -> Element<'_, Message> {
+        let visualize_button = button("Press me!").on_press(Message::ButtonPressed);
+        let number_input = text_input("Enter your on number or select from the below list", &self.number)
+            .on_input(Message::ContentChanged);
+        // let number_selector =combo_box(state, placeholder, selection, on_selected)
 
-        Column::new()
+        let column = Column::new()
             .align_x(iced::Center)
             .spacing(20)
+            .push(number_input)
             .push(Text::new(format!("Button pressed: {}", self.count)))
-            .push(button)
+            .push(visualize_button);
+
+        // let number_canvas = canvas();
+
+        Row::new()
+            .align_y(Center)
+            .spacing(20)
+            .push(column)
+            // .push(number_canvas)
             .into()
     }
 }
