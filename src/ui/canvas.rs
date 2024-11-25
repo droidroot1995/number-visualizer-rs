e771a9029f37a495e76f4cbbc7a4a52717f5cadc
Alexander Goryakin <goryakin@phystech.edu>
2024-11-26 00:35:53 +0300

    canvas init

--- src/ui/canvas.rs
+++ src/ui/canvas.rs
@@ -1,9 +1,10 @@
 // use iced::widget::canvas;
+use iced::Color;
 use std::collections::HashMap;
 
 pub struct Canvas {
-    colors: HashMap<String, String>,
-    degrees: HashMap<String, i8>,
+    colors: Vec<String>,
+    degrees: HashMap<String, i16>,
 }
 
 #[derive(Debug, Clone)]
@@ -23,8 +24,29 @@ impl Canvas {
 
 impl Default for Canvas {
     fn default() -> Self {
-        let colors = HashMap::<String, String>::new();
-        let degrees = HashMap::<String, i8>::new();
+        let colors = vec![
+            "D9CD85".to_string(),
+            "C5B884".to_string(),
+            "CC8C8D".to_string(),
+            "9E5872".to_string(),
+            "935771".to_string(),
+            "997897".to_string(),
+            "868291".to_string(),
+            "6A878B".to_string(),
+            "87AC9B".to_string(),
+            "B9BEA7".to_string()
+        ];
+        
+        let mut degrees = HashMap::<String, i16>::new();
+
+        let slice_degrees = 360 / 10;
+        let mut counter = 0;
+        while counter < 10 {
+            degrees.insert(counter.to_string(), slice_degrees * counter);
+            counter = counter + 1;
+        }
+
+        println!("Colors: {:?}, degrees: {:?}", colors.clone(), degrees.clone());
 
         Self {
             colors: colors,
